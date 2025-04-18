e6f081942af907ce65f9c1dc10596fd55876df05
Firsdv <firsdv8@gmail.com>
2025-04-18 16:50:22 +0300

    Приложение работает4 - убрал @Transactaional

--- src/main/java/com/example/crud_Spring_Boot/service/UserServiceImpl.java
+++ src/main/java/com/example/crud_Spring_Boot/service/UserServiceImpl.java
@@ -17,31 +17,31 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-    @Transactional
+
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
-    @Transactional(readOnly = true)
+
     @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
-    @Transactional(readOnly = true)
+
     @Override
     public User getUser(long id) {
         return userDao.getUser(id);
     }
 
-    @Transactional
+
     @Override
     public void deleteUser(long id) {
         userDao.deleteUser(id);
     }
 
-    @Transactional
+
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
